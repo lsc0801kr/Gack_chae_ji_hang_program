fc928f4aae4dc89f059a66cad31ed1b75b57ae27
lsc0801kr <74335091+lsc0801kr@users.noreply.github.com>
2021-03-31 12:58:57 +0900

    210331

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,37 +1,87 @@
 #include <iostream>
-using namespace std;
+//int main()
+//{
+	//포인터
+	//예제 4.7
+	//포인터의 덧셈과 뺄셈은 숫자의 증가, 감소가 아닌 포인터가 가리키는 위치를 이동시킴!
+	//short a[3] = { 0,1,2 }; //3개의 short형 정수가 메모리에 저장
+	//int b[3] = { 0,3,6 };
+	//cout << " a => " << a << endl;
+	//cout << " a+1 => " << a + 1 << endl;
+	//cout << " a+2 => " << a + 2 << endl;
 
-int main()
-{
-	/*int a = 10;
-	cout << "a => " << a << endl;
-	cout << "&a => " << &a << endl;
-	cout << "*&a => " << *&a << endl;*/
+	//cout << " b => " << b << endl;
+	//cout << " b+1 => " << b + 1 << endl;
+	//cout << " b+2 => " << b + 2 << endl;
+
+	//cout << " b[0] => " << b[0] << " *b => " << *b << endl;
+	//cout << " b[1] => " << b[1] << " *b+1 => " << *b + 1 << endl;
+	//cout << " b[1] => " << b[1] << " *(b+1) => " << *(b+1) << endl;
+
+	//예제 4.8
+	//int a[2] = { 10,20 };
+	//int* pa = a; //pa = a
+	//cout << a[0] << endl;
+	//cout << *(a + 1) << endl; //*(a + 1)==a[1]
+	//cout << pa[0] << endl;
+	//cout << *(pa + 1) << endl;
+	//===============================================
+	//int a[8] = { 10, 20, 30, 40, 50, 60, 70, 80 };
+	//int* pa = a; //a==&a[0]
+
+	////배열을 포인터 처럼 활용
+	//cout << a[0] << endl;
+	//cout << *(a + 1) << endl;
+	////배열이름이지만 포인트 변수처럼사용
+	////메모리 첫번째 공간 다음의 메모리 주소가 들어가지고 *를 써 숫자나옴
+	//cout << *(&a[0] + 1) << endl;
+	//cout << "\n\n\n";
+
+	////point를 배열처럼 활용
+	//cout << pa[0] << endl; //포인터를 배열처럼 사용
+	//cout << *(pa + 1) << endl; //포인터를 포인터 처럼 사용
+	//cout << pa[1] << endl;
+	//=====================================================
+	/*cout << "\n\n\n";
+	cout << "Array size is " << sizeof(a) << endl;
+	cout << "Integer size is " << sizeof(int) << endl;
+	cout << "Element #(숫자) is " << sizeof(a) / sizeof(int) << endl;
+	cout << "\n\n\n";*/
+	//=======================================================
+	////배열에 저장된 원소의 개수
+	//int s = sizeof(a) / sizeof(int);  //8
+
+	////"배열에 저장된 원소의 개수" 정보를 이용하여, 배열로 원소를 출력
+	//for (int i = 0; i < s; i++) {
+	//	cout << a[i] << "  ";
+	//}
+	//cout << "\n\n\n";
+	////"배열에 저장된 원소의 개수" 정보를 이용하여, 포인터로 원소를 출력
+	//for (int i = 0; i < s; i++) {
+	//	cout << *(pa + i) << "  "; //pa=a
+	//}
+	//cout << "\n\n\n";
+	////"배열에 저장된 원소의 개수" 정보를 이용하여, 배열을 포인터처럼 이용하여 원소를 출력
+	//for (int i = 0; i < s; i++) {
+	//	cout << *(a + i) << "  "; //pa=a
+	//}
+//=============================================================
+	/*int global_a = 0;
+	int first_function(int i)
+	{
+		return i * 100;
+	}
+	int main()
+	{
+		using namespace std;
+		int a = 1, b = 0;
+		b = first_function(a);
+			cout << b << endl;
+		return 0;
+	}*/
+//==============================================================
 
 
-	//예제 4.5
-	//int a = 10; //int 변수 a 선언 후, 10 할당
-	//int* p; //포인터 변수 p선언: int* (p)
-	//p = &a; //초기화: int* p =&a
-
-	//cout << "a => " << a << endl; //a변수 값
-	//cout << "&a => " << &a << endl; //a변수가 저장된 메모리 주소
-	//cout << "p => " << p << endl; //p변수값=a의 메모리 주소
-	//cout << "&p =>" << &p << endl; //p변수가 저장된 메모리 주소
-	//cout << "*p => " << *p << endl; //p변수에 저장된 메모리 주소가 가르키는 a변수값
-	
-	//예제 4.6
-	//int a = 10, b = 20;
-	//int* p = &a; //"p" linked with "a" 포인터 변수 선언 a변수의 주소를 p에 할당
-	//cout << " a => " << a << " b => " << b << endl; // a=10, b=10
-	//cout << " *p => " << *p << endl; //*p==a==*(&a)=10 a에 저장되어 있는 값 출력
-
-	//b = *p; // b with 10 assigned from *p=*(&a)=10 p에서 가리키는 변수 값을 b에 할당
-	//cout << " a => " << a << " b => " << b << endl; //a=10, b*p=*(&a)=10
-	//cout << " *p => " << *p << endl; //*p ==*(&a)=10
-
-	//*p = 30; //*p ==*(&a)=30 a변수 공간에 30할당
-	//cout << " a => " << a << " b => " << b << endl; //a=30, b=10
-	//cout << " *p => " << *p << endl; //*p=30
-	return 0;
-}
+//
+//	return 0;
+//}
